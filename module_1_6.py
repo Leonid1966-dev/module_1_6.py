35208b2048287f600b552608b09d018d79413182
Leonid1966-dev <rskle66@gmail.com>
2024-11-17 13:49:32 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+my_dict = {'Leonid':1966, 'Irina':1963, 'Marina':1987}
+print(my_dict)
+print(my_dict['Leonid'])
+my_dict['Nika'] = 2015
+print(my_dict)
+my_dict.update({'Sahsa':2019,
+                'Ivan':1984})
+print(my_dict)
+a = my_dict.pop('Ivan')
+print(a)
+print(my_dict)
+print()
+my_set = {5, 'Вишня', 3.14, 'Вишня', 5}
+print(my_set)
+my_set.add(8)
+print(my_set)
+my_set.add('Яблоко')
+print(my_set)
+my_set.remove(3.14)
+print(my_set)
+
